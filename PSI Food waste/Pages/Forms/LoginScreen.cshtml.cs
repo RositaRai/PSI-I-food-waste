8922476c52f572a272c65ecb07233c1fb7ebd5ef
Jaunius Tamulevičius <73691989+Jaunius343@users.noreply.github.com>
2021-10-06 13:13:54 +0300

    Feat/login verification (#8)
    * create new page for verified restaurants
    
    * add login screen with login verification
    
    * add prettier buttons
    
    * change button text font
    
    * add logout button + login check
    
    -button for logout
    -some pages require login to access
    -some pages check login before confirming action
    
    * add one more user login
    
    * fix username initation
    
    * fix return (lost during merge)
    
    * update button size

---
+++ PSI Food waste/Pages/Forms/LoginScreen.cshtml.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PSI_Food_waste.Models;
+
+namespace PSI_Food_waste.Pages.Forms
+{
+    public class LoginScreenModel : PageModel
+    {
+        [BindProperty]
+        public User NewUser { get; set; }
+
+        [BindProperty]
+        public string Msg { get; set; }
+
+        public List<User> users = new List<User>
+        {
+            new User {Username = "admin", Password = "admin"},
+            new User {Username = "abc", Password = "123"}
+        };
+
+        public void OnGet()
+        {
+            Msg = "";
+        }
+
+        public bool flag;
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            flag = false;
+            foreach(User user in users)
+            {
+                if (NewUser.Equals(user))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+            {
+                HttpContext.Session.SetString("username", NewUser.Username);
+                return RedirectToPage("/Index");
+            }
+            else
+            {
+                Msg = "Incorrect credentials";
+                return Page();
+            }
+        }
+    }
+}
