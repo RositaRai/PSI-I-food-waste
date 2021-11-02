e66e4172b261ae1d58840c298f63942a9366cc5d
Jaunius343 <73691989+Jaunius343@users.noreply.github.com>
2021-11-02 17:26:40 +0200

    add interface for ProductService

---
+++ PSI Food waste/Models/IProductRepository.cs
@@ -0,0 +1,26 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PSI_Food_waste.Models
+{
+    public interface IProductRepository
+    {
+        public List<Product> GetList(int id);
+        public void SortProducts();
+
+        public void Add(Product product);
+
+        public void Delete(int id);
+
+        public List<Product> GetAll();
+
+        public void SetAll(List<Product> products);
+
+        public void NewPrice(Product product);
+    }
+}
