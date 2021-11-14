cc3c51766e82d6696350ba1c16e2770515005040
Jaunius343 <73691989+Jaunius343@users.noreply.github.com>
2021-11-14 20:15:11 +0200

    test async / await

--- PSI Food waste/Pages/Forms/Sandbox.cs
+++ PSI Food waste/Pages/Forms/Sandbox.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -26,11 +27,68 @@ namespace PSI_Food_waste.Pages.Forms
             _restaurantRepository = restaurantRepository;
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
             ViewData["User"] = HttpContext.Session.GetString(key: "username");
             restaurants = _restaurantRepository.GetAll();
             products = _productRepository.GetAll();
+        }
+
+        [BindProperty]
+        public string Msg1 { get; set; }
+        public string Msg2 { get; set; }
+        //public PageResult OnPost(int id)
+        //{
+        //    if (id == 1)
+        //    {
+        //        Msg = "1";
+        //    }
+        //    else if (id == 2)
+        //    {
+        //        Msg = "2";
+        //    }
+        //    else Msg = "0";
+        //    return Page();
+        //}
+
+        public void OnPostSelect1()
+        {
+            Msg1 = "waiting";
+            SetLabelSync();
+        }
+
+        public async Task OnPostSelect2()
+        {
+            Msg2 = "2";
+            await SetLabelAwait();
+            //return Page();
+        }
+
+        public void OnPostSelect3()
+        {
+            Msg2 = "3";
+        }
+
+
+        private void SetLabelSync()
+        {
+            Msg1 = Calculate("Sync");
+        }
+        private string Calculate(string msg)
+        {
+            Thread.Sleep(1000);
+            return msg;
+        }
+
+
+        private async Task SetLabelAwait()
+        {
+            Msg1 = await CalculateAsync("Await");
+        }
+
+        private Task<string> CalculateAsync(string msg)
+        {
+            return Task.Run(() => Calculate(msg));
         }
     }
 }
