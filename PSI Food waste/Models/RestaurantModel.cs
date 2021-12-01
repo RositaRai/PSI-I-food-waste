9722a72f1c428ca820c7a544dbeb51336a33ec6b
Jaunius343 <73691989+Jaunius343@users.noreply.github.com>
2021-12-01 11:48:09 +0200

    fix subscriptions

--- PSI Food waste/Models/RestaurantModel.cs
+++ PSI Food waste/Models/RestaurantModel.cs
@@ -8,13 +8,38 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PSI_Food_waste.Models
 {
-    public class Restaurant 
+    public class Restaurant
     {
         public int Id { get; set; }
         [Required]
-        public string Title {  get; set;}
-        public string City {  get; set; }
-        public string Adress {  get; set;}
+        public string Title { get; set; }
+        public string City { get; set; }
+        public string Adress { get; set; }
 
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Restaurant otherRestaurant = obj as Restaurant;
+            if (otherRestaurant != null)
+                return this.Id.CompareTo(otherRestaurant.Id);
+            else
+                throw new ArgumentException("Object is not a Restaurant");
+        }
+
+        protected bool Equals(Restaurant other)
+        {
+            return Id == other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+                return false;
+            if (ReferenceEquals(this, obj))
+                return true;
+            var other = obj as Restaurant;
+            return other != null && Equals(other);
+        }
     }
 }
