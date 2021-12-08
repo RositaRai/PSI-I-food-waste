86b49859aaa4ca767364289e0bf539f382e3dba3
Jaunius343 <73691989+Jaunius343@users.noreply.github.com>
2021-12-08 13:39:46 +0200

    update initial db

--- PSI Food waste/Data/DbInitializer.cs
+++ PSI Food waste/Data/DbInitializer.cs
@@ -20,12 +20,12 @@ namespace PSI_Food_waste.Data
 
             var products = new Product[]
             {
-                new Product{RestId = 1, Name = "Product_1", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 10, Discount = 0},
-                new Product{RestId = 1, Name = "Product_2", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 10, Discount = 0},
-                new Product{RestId = 1, Name = "Product_3", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 10, Discount = 0},
-                new Product{RestId = 2, Name = "Product_4", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 10, Discount = 0},
-                new Product{RestId = 2, Name = "Product_5", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 10, Discount = 0},
-                new Product{RestId = 2, Name = "Product_6", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 10, Discount = 0}
+                new Product{RestId = 1, Name = "Margarita", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 5, Discount = 50},
+                new Product{RestId = 1, Name = "Italiska", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 5, Discount = 50},
+                new Product{RestId = 1, Name = "Havaju", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 5, Discount = 50},
+                new Product{RestId = 2, Name = "Burger", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 5, Discount = 50},
+                new Product{RestId = 2, Name = "Fries", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 5, Discount = 50},
+                new Product{RestId = 2, Name = "Big burger", Size=ProductSize.Small, IsGlutenFree = false, Price = 10, DiscountedPrice = 5, Discount = 50}
             };
 
             context.Products.AddRange(products);
@@ -33,8 +33,8 @@ namespace PSI_Food_waste.Data
 
             var restaurants = new Restaurant[]
             {
-                new Restaurant {Title = "Restaurant_1", City = "Kaunas", Adress = "Kauno g. 15"},
-                new Restaurant {Title = "Restaurant_2", City = "Vilnius", Adress = "Vilniaus g. "}
+                new Restaurant {Title = "Burger street ", City = "Vilnius", Adress = "Vilniaus g. ", PictureUrl = "https://imageproxy.wolt.com/venue/60a1f821c6df001844f76fed/10a81d36-b6cd-11eb-858e-ce279a5c61e0_men.jpeg"},
+                new Restaurant {Title = "Cili pica", City = "Kaunas", Adress = "Kauno g. 15", PictureUrl = "https://imageproxy.wolt.com/venue/5c88ebd41df13b000a63805e/1273afe269cd3ae5d49b04d813a1f4e8"}
             };
 
             context.Restaurants.AddRange(restaurants);
